b35d89bb5edf6267666133dd6395d10c33a44199
BossBele <adambeleko@gmail.com>
2023-03-16 17:57:29 +0300

    linting and error handling

--- auth.js
+++ auth.js
@@ -1,27 +1,16 @@
 import axios from 'axios';
 
 export default async function getToken(clientID, clientSecret) {
-  try {
-    // get token
-    const { data: { access_token: newToken, expires_in: expiresIn } } = await axios.get(`${process.env.MARRA_API}/oauth/token`, {
-      params: {
-        client_id: clientID,
-        client_secret: clientSecret,
-      },
-    });
+	// get token
+	const { data: { access_token: newToken, expires_in: expiresIn } } = await axios.get(`${process.env.MARRA_API}/oauth/token`, {
+		params: {
+			client_id: clientID,
+			client_secret: clientSecret,
+		},
+	});
 
-    if (!newToken) {
-      return res.status(401).send({
-        code: 'RESTRICTED',
-        message: 'Unauthorized',
-      });
-    }
+	const milliseconds = Number(expiresIn) * 1000;
+	const newExpiryDate = new Date(Date.now() + milliseconds);
 
-    const milliseconds = Number(expiresIn) * 1000;
-    const newExpiryDate = new Date(Date.now() + milliseconds);
-
-    return [newExpiryDate, newToken];
-  } catch (error) {
-    return res.status(500).send({ code: 'SERVER_ERROR', message: 'Internal server error' });
-  }
+	return [newExpiryDate, newToken];
 }
