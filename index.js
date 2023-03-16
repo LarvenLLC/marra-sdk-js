c9491fe6d44556e475b66671a9d835408e1e762b
BossBele <adambeleko@gmail.com>
2023-03-16 16:43:22 +0300

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+import { secureRequets } from "./api";
+
+/**
+ * Initialize Marra SDK
+ * @param {Config} configuration - Configuration with API keys to initialize SDK
+ */
+export default function Marra({ clientID, clientSecret }) {
+  secureRequets(clientID, clientSecret);
+}
